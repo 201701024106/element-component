da590d8546436d2c943dee0a0f3c5f76309ac433
wangdongsheng <201647997@qq.com>
2025-11-17 17:14:27 +0800

    core：路由配置优化

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,139 +2,94 @@ import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import container from "@/components/container/src/index.vue";
 
 const routes: RouteRecordRaw[] = [
-  {
-    path: '/', component: container,
-    children: [
-      {
-        path: '',
-        component: () => import('@/pages/Home.vue'),
-        meta: {
-          title: '首页',
-          icon: 'el-icon-rice-ag'
-        }
-      },
-    ]
-  },
-  {
-    path: '/icon-select', component: container,
-    children: [
-      {
-        path: '',
-        component: () => import('@/pages/IconSelect.vue'),
-        meta: {
-          title: '图标选择器',
-          icon: 'el-icon-rice-ag'
-        }
-      },
-    ]
-  },
-  {
-    path: '/trend', component: container,
-    children: [
-      {
-        path: '',
-        component: () => import('@/pages/Trend.vue'),
-        meta: {
-          title: '图标选择器',
-          icon: 'el-icon-rice-ag'
-        }
-      },
-    ]
-  },
-  {
-    path: '/notification', component: container,
-    children: [
-      {
-        path: '',
-        component: () => import('@/pages/Notification.vue'),
-        meta: {
-          title: '通知菜单',
-          icon: 'el-icon-rice-ag'
-        }
-      },
-    ]
-  },
-  {
-    path: '/leftmenu', component: container,
-    children: [
-      {
-        path: '',
-        component: () => import('@/pages/Leftmenu.vue'),
-        meta: {
-          title: '左侧菜单',
-          icon: 'el-icon-rice-ag'
-        }
-      },
-    ]
-  },
-  {
-    path: '/progress', component: container,
-    children: [
-      {
-        path: '',
-        component: () => import('@/pages/Progress.vue'),
-        meta: {
-          title: '进度条',
-          icon: 'el-icon-rice-ag'
-        }
-      },
-    ]
-  },
-  {
-    path: '/timeRange', component: container,
-    children: [
-      {
-        path: '',
-        component: () => import('@/pages/TimeRange.vue'),
-        meta: {
-          title: '时间选择器',
-          icon: 'el-icon-rice-ag'
-        }
-      },
-    ]
-  },
-  {
-    path: '/dateRange', component: container,
-    children: [
-      {
-        path: '',
-        component: () => import('@/pages/DateRange.vue'),
-        meta: {
-          title: '日期选择器',
-          icon: 'el-icon-rice-ag'
-        }
-      },
-    ]
-  },
-  {
-    path: '/citySelect', component: container,
-    children: [
-      {
-        path: '',
-        component: () => import('@/pages/citySelect.vue'),
-        meta: {
-          title: '城市选择器',
-          icon: 'el-icon-rice-ag'
-        }
-      },
-    ]
-  },
-  {
-    path: '/calendar', component: container,
-    children: [
-      {
-        path: '',
-        component: () => import('@/pages/Calendar.vue'),
-        meta: {
-          title: '日历选择器',
-          icon: 'el-icon-rice-ag'
-        }
-      },
-    ]
-  },
+    {
+        path: '/', component: container,
+        children: [
+            {
+                path: '',
+                component: () => import('@/pages/Home.vue'),
+                meta: {
+                    title: '首页',
+                    icon: 'el-icon-rice-ag'
+                }
+            },
+            {
+                path: '/icon-select',
+                component: () => import('@/pages/IconSelect.vue'),
+                meta: {
+                    title: '图标选择器',
+                    icon: 'el-icon-rice-ag'
+                }
+            },
+            {
+                path: '/trend',
+                component: () => import('@/pages/Trend.vue'),
+                meta: {
+                    title: '图标选择器',
+                    icon: 'el-icon-rice-ag'
+                }
+            },
+            {
+                path: '/notification',
+                component: () => import('@/pages/Notification.vue'),
+                meta: {
+                    title: '通知菜单',
+                    icon: 'el-icon-rice-ag'
+                }
+            },
+            {
+                path: '/leftmenu',
+                component: () => import('@/pages/Leftmenu.vue'),
+                meta: {
+                    title: '左侧菜单',
+                    icon: 'el-icon-rice-ag'
+                }
+            },
+            {
+                path: '/progress',
+                component: () => import('@/pages/Progress.vue'),
+                meta: {
+                    title: '进度条',
+                    icon: 'el-icon-rice-ag'
+                }
+            },
+            {
+                path: '/timeRange',
+                component: () => import('@/pages/TimeRange.vue'),
+                meta: {
+                    title: '时间选择器',
+                    icon: 'el-icon-rice-ag'
+                }
+            },
+            {
+                path: '/dateRange',
+                component: () => import('@/pages/DateRange.vue'),
+                meta: {
+                    title: '日期选择器',
+                    icon: 'el-icon-rice-ag'
+                }
+            },
+            {
+                path: '/citySelect',
+                component: () => import('@/pages/CitySelect.vue'),
+                meta: {
+                    title: '城市选择器',
+                    icon: 'el-icon-rice-ag'
+                }
+            },
+            {
+                path: '/calendar',
+                component: () => import('@/pages/Calendar.vue'),
+                meta: {
+                    title: '日历选择器',
+                    icon: 'el-icon-rice-ag'
+                }
+            },
+        ]
+    },
 ];
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
+    history: createWebHistory(),
+    routes,
 });
 export default router;
