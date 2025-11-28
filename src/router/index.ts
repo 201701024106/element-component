c7d11929cc6afa51537bee34b9c2cc773f1eef3f
wangdongsheng <201647997@qq.com>
2025-11-28 14:24:42 +0800

    core: 路由路径问题修复

--- src/router/index.ts
+++ src/router/index.ts
@@ -105,7 +105,7 @@ const routes: RouteRecordRaw[] = [
     },
 ];
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHistory(import.meta.env.BASE_URL),
     routes,
 });
 export default router;
